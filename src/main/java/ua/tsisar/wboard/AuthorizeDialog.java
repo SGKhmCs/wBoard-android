a7e896ae2d39709d916e5da69a6d304483901e0f
Tsisar <fhantom.tp@gmail.com>
2017-06-19 11:43:38 +0300

    request to dialog

--- src/main/java/ua/tsisar/wboard/AuthorizeDialog.java
+++ src/main/java/ua/tsisar/wboard/AuthorizeDialog.java
@@ -8,9 +8,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class AuthorizeDialog extends DialogFragment {
@@ -18,17 +23,19 @@ public class AuthorizeDialog extends DialogFragment {
     private EditText usernameEditText;
     private EditText passwordEditText;
     private CheckBox rememberMeCheckBox;
-    private ICredentialsDialogListener listener;
+    private IAuthorizeDialogListener listener;
 
-    public interface ICredentialsDialogListener {
-        void onDialogPositiveClick(String username, String password, boolean rememberMe);
+    private final String TAG = "myLogs";
+
+    public interface IAuthorizeDialogListener {
+        void onAuthorized(boolean rememberMe);
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (getActivity() instanceof ICredentialsDialogListener) {
-            listener = (ICredentialsDialogListener) getActivity();
+        if (getActivity() instanceof IAuthorizeDialogListener) {
+            listener = (IAuthorizeDialogListener) getActivity();
         }
     }
 
@@ -42,17 +49,58 @@ public class AuthorizeDialog extends DialogFragment {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
                 .setView(view)
-                .setTitle("Sign In")
+                .setTitle("Authorize")
                 .setNegativeButton("Cancel", null)
                 .setPositiveButton("Continue", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        if (listener != null) {
-                            listener.onDialogPositiveClick(usernameEditText.getText().toString(),
-                                    passwordEditText.getText().toString(), rememberMeCheckBox.isChecked());
-                        }
+                        authorize(
+                                usernameEditText.getText().toString(),
+                                passwordEditText.getText().toString(),
+                                rememberMeCheckBox.isChecked()
+                        );
                     }
                 });
         return builder.create();
     }
+
+    private void authorize(String stringUsername,
+                           String stringPassword, boolean booleanRememberMe){
+        Authorize authorize = new Authorize();
+        authorize.setUsername(stringUsername);
+        authorize.setPassword(stringPassword);
+        authorize.setRememberMe(booleanRememberMe);
+
+        Call<Token> tokenCall = App.getApi().authorize(authorize);
+        tokenCall.enqueue(new Callback<Token>() {
+            @Override
+            public void onResponse(Call<Token> call, Response<Token> response) {
+                Log.d(TAG, "Response: " + response.isSuccessful() + ", code: " + response.code());
+                switch (response.code()){
+                    case 200:
+                        App.setIdToken(response.body().getIdToken());
+                        if (listener != null) {
+                            listener.onAuthorized(rememberMeCheckBox.isChecked());
+                        }
+                        break;
+                    case 201:
+                        break;
+                    case 401:
+                        break;
+                    case 403:
+                        break;
+                    case 404:
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Token> call, Throwable throwable) {
+
+            }
+        });
+    }
+
 }
--- src/main/java/ua/tsisar/wboard/MainActivity.java
+++ src/main/java/ua/tsisar/wboard/MainActivity.java
@@ -12,7 +12,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements AuthorizeDialog.ICredentialsDialogListener {
+public class MainActivity extends AppCompatActivity implements AuthorizeDialog.IAuthorizeDialogListener {
 
     private final String TAG = "myLogs";
     private final String TOKEN = "token";
@@ -45,13 +45,13 @@ public class MainActivity extends AppCompatActivity implements AuthorizeDialog.I
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_authorize:
-                showAuthenticateDialog();
+                showAuthorizeDialog();
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void showAuthenticateDialog() {
+    private void showAuthorizeDialog() {
         AuthorizeDialog dialog = new AuthorizeDialog();
         Bundle arguments = new Bundle();
         dialog.setArguments(arguments);
@@ -60,41 +60,8 @@ public class MainActivity extends AppCompatActivity implements AuthorizeDialog.I
     }
 
     @Override
-    public void onDialogPositiveClick(String username, String password, boolean rememberMe) {
-        authorize(username, password, rememberMe);
-    }
-
-    //TODO перенести в діалог, відпрацювати помилку авторизації, з діалогу повертати токен, перейменувати діалог
-    private void authorize(String stringUsername,
-                           String stringPassword, final boolean booleanRememberMe){
-        Authorize authorize = new Authorize();
-        authorize.setUsername(stringUsername);
-        authorize.setPassword(stringPassword);
-        authorize.setRememberMe(booleanRememberMe);
-
-        Call<Token> tokenCall = App.getApi().authorize(authorize);
-        tokenCall.enqueue(new Callback<Token>() {
-            @Override
-            public void onResponse(Call<Token> call, Response<Token> response) {
-
-                //-----------------------TEST------------------------//
-                String string = response.toString();
-                Log.d(TAG, "Response: " + response.isSuccessful());
-                if(response.isSuccessful()) {
-                    String idToken = response.body().getIdToken();
-                    App.setIdToken(idToken);
-
-                    Log.d(TAG, "Token: " + idToken);
-                }
-                //---------------------------------------------------//
-            }
-
-            @Override
-            public void onFailure(Call<Token> call, Throwable throwable) {
-
-            }
-        });
-        if(booleanRememberMe) {
+    public void onAuthorized(boolean rememberMe) {
+        if(rememberMe){
             saveIdToken(App.getToken().getIdToken());
         }
     }
