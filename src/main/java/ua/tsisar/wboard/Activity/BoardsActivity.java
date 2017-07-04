34c61023b395bfcd19e6ef49dc6799ea9a833600
Tsisar <fhantom.tp@gmail.com>
2017-07-04 16:23:22 +0300

    add search view

--- src/main/java/ua/tsisar/wboard/Activity/BoardsActivity.java
+++ src/main/java/ua/tsisar/wboard/Activity/BoardsActivity.java
@@ -1,7 +1,13 @@
 package ua.tsisar.wboard.Activity;
 
+import android.app.SearchManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.List;
 
@@ -14,11 +20,15 @@ import ua.tsisar.wboard.R;
 public class BoardsActivity extends BoardActivitySuper {
 
     private static final int RESPONSE_OK = 200;
+    private static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boards);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         final BoardService boardService = new  BoardService(this);
         boardService.getAllBoards(0, 3, "name");
@@ -26,6 +36,36 @@ public class BoardsActivity extends BoardActivitySuper {
         boardService.getBoard(951);
         BoardDTO boardDTO = new BoardDTO(952, "brdUUUU", true);
         boardService.updateBoard(boardDTO);
+        String s = boardDTO.toString();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        if(null!=searchManager ) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        }
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d(TAG, "onQueryTextSubmit: " + query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(TAG, "onQueryTextChange: " + newText);
+                return false;
+            }
+        });
+        return true;
     }
 
     @Override
